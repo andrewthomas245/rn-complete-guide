af7980ee64791c321dd6029c84b5b80633b9ae0a
KyawHtutNaing245 <KyawHtutNaing245@github.com>
2021-09-06 20:29:44 +0630

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  //declare useState
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  //function to add enter goal into currentGoals
+  const addGoHandler = (enteredGoal) => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: enteredGoal },
+    ]);
+  };
+
+  const removeGoHanlder = (goalID) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalID);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <GoalInput onAddGo={addGoHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoHanlder}
+            title={itemData.item.value}
+          />
+        )}
+      ></FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  myHeaderStyle: {
+    color: "white",
+    backgroundColor: "#3283fc",
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+    borderWidth: 0,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  screen: {
+    padding: 40,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: "#c0ede2",
+    borderColor: "blue",
+    borderWidth: 1,
+  },
+  buttonStyle: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
   },
 });
